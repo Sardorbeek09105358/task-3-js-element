afb73b98826020d3edbc678a6591ffde085b7fb9
FIRST_NAME <MY_NAME@example.com>
2023-11-21 17:00:40 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let width = document.getElementById("width");
+let height = document.getElementById("height");
+let radius = document.getElementById("radius");
+let color = document.getElementById("color");
+let box = document.getElementById("box");
+let ok = document.getElementById("ok");
+
+ok.addEventListener("click", ()=>{
+    box.style.width = width.value + "px"
+    box.style.height = height.value + "px"
+    box.style.borderRadius = radius.value + "px"
+    box.style.backgroundColor = color.value 
+});
+
+
+
+
+
+
+
